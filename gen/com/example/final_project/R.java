18e718b9f84e735a43919ff2dd4fe4253f597051
APTX4869 <npes_87184@hotmail.com>
2014-08-02 19:49:58 +0800

    add version name in about dialog

--- gen/com/example/final_project/R.java
+++ gen/com/example/final_project/R.java
@@ -167,17 +167,18 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f05000a;
-        public static final int container=0x7f050007;
-        public static final int listview1=0x7f050008;
+        public static final int action_settings=0x7f05000b;
+        public static final int container=0x7f050008;
+        public static final int listview1=0x7f050009;
         public static final int spb_interpolator_accelerate=0x7f050000;
         public static final int spb_interpolator_acceleratedecelerate=0x7f050002;
         public static final int spb_interpolator_decelerate=0x7f050003;
         public static final int spb_interpolator_linear=0x7f050001;
         public static final int textView1=0x7f050004;
         public static final int textView2=0x7f050005;
-        public static final int textView3=0x7f050006;
-        public static final int webView1=0x7f050009;
+        public static final int textView3=0x7f050007;
+        public static final int textView4=0x7f050006;
+        public static final int webView1=0x7f05000a;
     }
     public static final class integer {
         public static final int spb_default_interpolator=0x7f080001;
